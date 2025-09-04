90f90ffb9dd46d7fac81fdba7542b955690a66c2
surafelhabte1 <surafelhabte1@gmail.com>
2025-09-04 17:36:29 +0300

    set db config file

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,8 @@
 import dotenv from "dotenv";
-import { PrismaClient } from "@prisma/client";
 import app from "./app";
 
 dotenv.config();
 
-const prisma = new PrismaClient();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {

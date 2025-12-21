c331bedf80e5a492948402412c1f41f04e8567f0
surafelhabte1 <surafelhabte1@gmail.com>
2025-12-21 18:30:37 +0300

    override parent create and update methods and use create_people and delete_people methods

--- src/repositories/staff.repositorie.ts
+++ src/repositories/staff.repositorie.ts
@@ -1,6 +1,5 @@
 import { Response } from "express";
-import { auth_signup, get_random_password } from "../routes/auth.route.ts";
-import { BaseRepository, prisma } from "./base.repositorie.ts";
+import { BaseRepository } from "./base.repositorie.ts";
 
 export class staffRepo extends BaseRepository<"staff"> {
   constructor() {
@@ -8,36 +7,10 @@ export class staffRepo extends BaseRepository<"staff"> {
   }
 
   async create(data: any, res: Response, signal?: AbortSignal) {
-    const { first_name, middle_name, email } = data;
-
-    const password = get_random_password();
-
-    return auth_signup({
-      data: { name: `${first_name} ${middle_name}`, email, password },
-      after_func: async ({ better_auth_id, tx }) => {
-        await tx.staff.create({
-          data: {
-            ...data,
-            better_auth_id,
-          },
-          signal,
-        });
-      },
-      res: res,
-      includePasswordInEmailTemplate: true,
-    });
+    return this.create_people(data, res, signal);
   }
 
   async delete(id: string, signal?: AbortSignal) {
-    try {
-      const staff = await this.model.findUnique({ where: { id }, signal });
-
-      return await prisma.$transaction(async (tx) => {
-        await tx.staff.delete({ where: { id } });
-        await tx.user.delete({ where: { id: staff.better_auth_id } });
-      });
-    } catch (error) {
-      this.handleError(error);
-    }
+    return this.delete_people(id, signal);
   }
 }

81a5953ff352983b1d0aa42a44d8c807adbe4b82
surafelhabte1 <surafelhabte1@gmail.com>
2025-12-22 14:54:04 +0300

    crud test : teacher

--- src/repositories/teacher.repositorie.ts
+++ src/repositories/teacher.repositorie.ts
@@ -1,7 +1,16 @@
+import { Response } from "express";
 import { BaseRepository } from "./base.repositorie.ts";
 
 export class teacherRepo extends BaseRepository<"teacher"> {
   constructor() {
     super("teacher");
+  }
+
+  async create(data: any, res: Response, signal?: AbortSignal) {
+    return this.create_people(data, res, signal);
+  }
+
+  async delete(id: string, signal?: AbortSignal) {
+    return this.delete_people(id, signal);
   }
 }

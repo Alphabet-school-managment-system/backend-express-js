be17c7dc81749b8f29705bb19db0f0a3489b5c5b
surafelhabte1 <surafelhabte1@gmail.com>
2026-02-27 19:28:46 +0300

    feat: add BetterAuthIdSchema for improved ID validation in BaseRouter

--- src/routes/student.route.ts
+++ src/routes/student.route.ts
@@ -1,21 +1,7 @@
 import { StudentController } from "../controllers/student.controller.js";
 import { studentSchema } from "../validators/zod.schema.js";
 import { BaseRouter } from "./base.router.js";
-import z from "zod";
 
-const studentFindOneIdSchema = z.object({
-  id: z.union([
-    z.uuid({ message: "Invalid UUID format" }),
-    z
-      .string()
-      .regex(/^[A-Za-z0-9]{32}$/, "Invalid Better Auth id format"),
-  ]),
-});
-
-const router = new BaseRouter(
-  new StudentController(),
-  studentSchema,
-  studentFindOneIdSchema,
-).router;
+const router = new BaseRouter(new StudentController(), studentSchema).router;
 
 export default router;

7fb528cf78edce956ac17e8c6632130aac3bb19d
surafelhabte1 <surafelhabte1@gmail.com>
2026-04-30 23:34:30 +0300

    feat: enhance behavior and enrollment schemas with optional fields and add search functionality in repositories

--- src/repositories/behavior.repositorie.ts
+++ src/repositories/behavior.repositorie.ts
@@ -1,7 +1,43 @@
 import { BaseRepository } from "./base.repositorie.js";
+import { Request } from "express";
 
 export class BehaviorRepository extends BaseRepository<"behavior"> {
   constructor() {
     super("behavior");
+  }
+
+  async search(req: Request) {
+    try {
+      const signal = (req as any).prismaSignal;
+      const queryOptions = this.preProcessSearchQuery(req.query);
+
+      return await this.model.findMany(
+        {
+          ...queryOptions,
+          include: {
+            enrollment: {
+              select: {
+                id: true,
+                grade: true,
+                section: true,
+                student: {
+                  select: {
+                    id: true,
+                    first_name: true,
+                    middle_name: true,
+                    last_name: true,
+                    image: true,
+                    student_registration_number: true,
+                  },
+                },
+              },
+            },
+          },
+        },
+        { signal },
+      );
+    } catch (error) {
+      this.handleError(error);
+    }
   }
 }
